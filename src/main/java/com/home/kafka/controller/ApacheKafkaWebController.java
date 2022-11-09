d02e0b957524847d178b300b9e3fac2abae0b8f2
srinik05 <srinivasu.jsb6@gmail.com>
2022-11-09 10:40:43 +0530

    springboot kafka example

---
+++ src/main/java/com/home/kafka/controller/ApacheKafkaWebController.java
@@ -0,0 +1,26 @@
+package com.home.kafka.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.home.kafka.service.KafkaSender;
+
+@RestController
+@RequestMapping(value = "/icici-kafka/")
+public class ApacheKafkaWebController {
+
+	@Autowired
+	KafkaSender kafkaSender;
+
+	@GetMapping(value = "/producer")
+	public String producer(@RequestParam("message") String message) {
+		kafkaSender.send(message);
+
+		return message +  " has received monthly salary Successfully";
+	}
+
+}
+
